153e9e377a6db1ffac263cea075fbdfcd52e5e0b
JosephUchennaOffiaeli <offiaelijoseph@gmail.com>
2026-06-04 19:40:05 +0100

    form

--- form/review.js
+++ form/review.js
@@ -4,24 +4,23 @@ const products = [
     { id: "jm1235s1", name: "Time Machine" }
 ];
 
-// Logic for Index Page
 const selectElement = document.getElementById("product");
 if (selectElement) {
-    products.forEach(p => {
-        let opt = document.createElement("option");
-        opt.value = p.id;
-        opt.textContent = p.name;
-        selectElement.appendChild(opt);
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        selectElement.appendChild(option);
     });
 
-    document.querySelector('form').addEventListener('submit', () => {
-        let count = parseInt(localStorage.getItem('reviewCount') || 0);
-        localStorage.setItem('reviewCount', count + 1);
+    const form = document.querySelector('form');
+    form.addEventListener('submit', () => {
+        let numReviews = parseInt(localStorage.getItem('reviewCount')) || 0;
+        localStorage.setItem('reviewCount', numReviews + 1);
     });
 }
 
-// Logic for Review Page
-const display = document.getElementById("review-count");
-if (display) {
-    display.textContent = localStorage.getItem('reviewCount') || 0;
+const countDisplay = document.getElementById("review-count");
+if (countDisplay) {
+    countDisplay.textContent = localStorage.getItem('reviewCount') || 0;
 }
